de9fea433002acd2dbf68b6725b0ae460e98183c
erhannis <Eyeillus@gmail.com>
2018-03-27 01:17:25 -0400

    Greatly improved handling of unassigned cells

--- src/main/java/com/erhannis/puzzlegen/Main.java
+++ src/main/java/com/erhannis/puzzlegen/Main.java
@@ -28,7 +28,7 @@ public class Main {
   public static void main(String[] args) throws IOException {
     long t = System.currentTimeMillis();
     //Collection<Cell> cells = Phase1CellGeneration.generateSquareBoard(50, 50);
-    Collection<Cell> cells = Phase1CellGeneration.generateHexBoard(50, 20, true);
+    Collection<Cell> cells = Phase1CellGeneration.generateHexBoard(60, 40, true);
     System.out.println("Phase 1 " + (System.currentTimeMillis() - t));
     
     t = System.currentTimeMillis();
@@ -36,7 +36,7 @@ public class Main {
     Set<Group> groups = null;
     for (int i = 0; i < 1; i++) {
       System.err.print(i + " ");
-      Set<Group> lGroups = Phase2Grouping.groupCellsDefault(cells.stream().findAny().get(), 25, 0);
+      Set<Group> lGroups = Phase2Grouping.groupCellsDefault(cells.stream().findAny().get(), 10, 0, Phase2Grouping.P2ResolutionMode.MAKE_OWN_GROUP_WITH_MIN_SIZE_20_ELSE_ASSIGN_TO_LEAST_CONTACT_SAVE_BORDER);
       System.err.println("");
       HashSet<Cell> lCells = new HashSet<Cell>();
       for (Group g : lGroups) {
--- src/main/java/com/erhannis/puzzlegen/phases/Phase2Grouping.java
+++ src/main/java/com/erhannis/puzzlegen/phases/Phase2Grouping.java
@@ -5,13 +5,17 @@
  */
 package com.erhannis.puzzlegen.phases;
 
+import com.erhannis.mathnstuff.FactoryHashMap;
 import com.erhannis.puzzlegen.structure.Cell;
+import com.erhannis.puzzlegen.structure.Face;
 import com.erhannis.puzzlegen.structure.Group;
 import java.security.SecureRandom;
 import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.HashSet;
+import java.util.Iterator;
 import java.util.Set;
+import java.util.function.BiConsumer;
 import java.util.function.Function;
 import java.util.stream.Collectors;
 
@@ -20,19 +24,25 @@ import java.util.stream.Collectors;
  * @author erhannis
  */
 public class Phase2Grouping {
-  public static Set<Group> groupCellsDefault(Cell root, int groupCount, int ignoreSourceNeighborRadius) {
+  public static enum P2ResolutionMode {
+    NONE, ASSIGN_TO_LEAST_CONTACT, ASSIGN_TO_LEAST_CONTACT_SAVE_BORDER, MAKE_OWN_GROUP, MAKE_OWN_GROUP_WITH_MIN_SIZE_20_ELSE_ASSIGN_TO_LEAST_CONTACT_SAVE_BORDER
+  }
+
+  private static final int MIN_RESOLUTION_GROUP_SIZE_FOR_MODE = 20;
+  
+  public static Set<Group> groupCellsDefault(Cell root, int groupCount, int ignoreSourceNeighborRadius, P2ResolutionMode resolutionMode) {
     final int GROUP_COUNT = groupCount;
     SecureRandom rand = new SecureRandom();
-    
+
     Set<Group> groups = new HashSet<>();
     HashMap<Cell, Group> c2g = new HashMap<>();
-    
+
     HashSet<Cell> allCells = new HashSet<>();
     Utils.forEachCell(root, c -> allCells.add(c));
-    
+
     ArrayList<Cell> growing = new ArrayList<>();
     HashSet<Cell> done = new HashSet<>();
-    
+
     ArrayList<Cell> potentialSeeds = new ArrayList<>(allCells);
     for (int i = 0; i < GROUP_COUNT; i++) {
       Cell c = potentialSeeds.remove(rand.nextInt(potentialSeeds.size()));
@@ -42,6 +52,7 @@ public class Phase2Grouping {
       g.cells.add(c);
       c2g.put(c, g);
     }
+    HashSet<Cell> unassignedCells = new HashSet<Cell>(potentialSeeds);
 
     //TODO This is complicated, and should be tested.
     Function<Cell, Set<Cell>> getNeighborsAvailableForExpansion = c -> {
@@ -63,7 +74,7 @@ public class Phase2Grouping {
       result = result.stream().filter(c2 -> !(c2.getAllNeighbors().stream().filter(c2n -> !ignore.contains(c2n) && !c2n.equals(c)).anyMatch(c2n -> c2g.get(c2n) == c2g.get(c)))).collect(Collectors.toSet()); // Remove if too close to own group, ignoring adj from c
       return result;
     };
-    
+
     while (!growing.isEmpty()) {
       int i = rand.nextInt(growing.size());
       Cell source = growing.get(i);
@@ -79,13 +90,125 @@ public class Phase2Grouping {
       g.cells.add(target);
       c2g.put(target, g);
       growing.add(target);
+      unassignedCells.remove(target);
     }
+
+    Function<Cell, Group> fillUnassigned = cell -> {
+      if (c2g.get(cell) != null) {
+        throw new IllegalArgumentException("Cell already has a group!");
+      }
+      Group g = new Group();
+      g.cells.add(cell);
+
+      HashSet<Cell> toDo0 = new HashSet<>();
+      HashSet<Cell> done0 = new HashSet<>();
+      toDo0.add(cell);
+      while (!toDo0.isEmpty()) {
+        Iterator<Cell> i = toDo0.iterator();
+        Cell c = i.next();
+        i.remove();
+
+        g.cells.add(c);
+
+        HashSet<Cell> lToDo = new HashSet<Cell>();
+        lToDo.addAll(c.getAdjacentNeighbors().stream().filter(c2 -> c2g.get(c2) == null).collect(Collectors.toSet()));
+        lToDo.removeAll(done0); // Hopefully this is smart about it
+        toDo0.addAll(lToDo);
+
+        done0.add(c);
+      }
+
+      return g;
+    };
+
+    Group border = new Group(); //TODO ???
     
+    BiConsumer<Group, Boolean> assignToLeastContact = (group, skipBorder) -> {
+      Set<Face> faces = group.cells.stream().flatMap(c2 -> c2.faces.stream()).collect(Collectors.toSet());
+      FactoryHashMap<Group, Integer> scores = new FactoryHashMap<Group, Integer>(g2 -> 0);
+      boolean hasBorderFace = false;
+      for (Face f : faces) {
+        if (f.cells.size() < 2) {
+          hasBorderFace = true;
+        }
+        for (Cell c2 : f.cells) {
+          Group g2 = c2g.get(c2);
+          if (g2 != null) {
+            scores.put(g2, scores.get(g2) + 1);
+          }
+        }
+      }
+      Group winner;
+      if (hasBorderFace && skipBorder) {
+        winner = border;
+      } else {
+        winner = scores.entrySet().stream().min((a, b) -> Integer.compare(a.getValue(), b.getValue())).get().getKey();
+      }
+
+      winner.cells.addAll(group.cells);
+      for (Cell c2 : group.cells) {
+        c2g.put(c2, winner);
+        unassignedCells.remove(c2);
+        done.add(c2);
+      }
+    };
+
+    switch (resolutionMode) {
+      case ASSIGN_TO_LEAST_CONTACT:
+      case ASSIGN_TO_LEAST_CONTACT_SAVE_BORDER:
+        while (!unassignedCells.isEmpty()) {
+          Iterator<Cell> i = unassignedCells.iterator();
+          Cell c = i.next();
+          i.remove();
+          Group gTemp = fillUnassigned.apply(c);
+          assignToLeastContact.accept(gTemp, resolutionMode == P2ResolutionMode.ASSIGN_TO_LEAST_CONTACT_SAVE_BORDER);
+        }
+        break;
+      case MAKE_OWN_GROUP:
+        while (!unassignedCells.isEmpty()) {
+          Iterator<Cell> i = unassignedCells.iterator();
+          Cell c = i.next();
+          i.remove();
+          Group gTemp = fillUnassigned.apply(c);
+
+          groups.add(gTemp);
+          for (Cell c2 : gTemp.cells) {
+            c2g.put(c2, gTemp);
+            unassignedCells.remove(c2);
+            done.add(c2);
+          }
+        }
+        break;
+      case MAKE_OWN_GROUP_WITH_MIN_SIZE_20_ELSE_ASSIGN_TO_LEAST_CONTACT_SAVE_BORDER:
+        while (!unassignedCells.isEmpty()) {
+          Iterator<Cell> i = unassignedCells.iterator();
+          Cell c = i.next();
+          i.remove();
+          Group gTemp = fillUnassigned.apply(c);
+
+          if (gTemp.cells.size() >= MIN_RESOLUTION_GROUP_SIZE_FOR_MODE) {
+            groups.add(gTemp);
+            for (Cell c2 : gTemp.cells) {
+              c2g.put(c2, gTemp);
+              unassignedCells.remove(c2);
+              done.add(c2);
+            }
+          } else {
+            assignToLeastContact.accept(gTemp, true);
+          }
+        }
+        break;
+      case NONE:
+      default:
+        // Nothing
+        break;
+    }
+
     if (done.size() != allCells.size()) {
       //throw new IllegalStateException("Darn, didn't get all the cells!");
       System.err.println("Only got " + done.size() + " / " + allCells.size() + " cells");
     }
-    
+
     return groups;
   }
 }
